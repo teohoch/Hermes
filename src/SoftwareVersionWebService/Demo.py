66d507ecc7ed2da87901435d0ac20e1a980a33aa
Teodoro Hochfarber <teohoch2@gmail.com>
2015-03-07 11:54:21 -0300

    Fixed the demo

--- src/SoftwareVersionWebService/Demo.py
+++ src/SoftwareVersionWebService/Demo.py
@@ -2,8 +2,11 @@ import  json
 import time
 
 import requests
+
+headers = {'Content-Type': 'application/json'}
 url = 'http://sstudent01.osf.alma.cl:800/version/'
-payload = {'ste': 'AOS', 'time': time.time(), 'architecture': 64}
-r = requests.post(url=url, data=json.dumps(payload))
+payload = dict(ste='AOS', time=time.time(), architecture=64)
+r = requests.post(url=url, data=json.dumps(payload), headers=headers)
+print r
 rj = r.json()
 print rj
